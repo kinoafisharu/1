15bbbfcb44cdd311b9275b8aa8157a5c3e7626a4
Иванов и Ко <32426965+kinoafisharu@users.noreply.github.com>
2020-07-13 16:06:52 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <HelloWorld msg="Вас приветствует среда для разработки vue компонентов"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+#app {
+  background: #000;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #fff;
+  height: 100vh;
+  padding: 100px;
+}
+</style>
